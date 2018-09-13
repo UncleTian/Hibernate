effb6aa2d2e68a554010dde7cbf23635634f45f5
tian.luolin <tianluolin_ustb@163.com>
2018-09-13 17:40:26 +0800

    add evict test case

--- test-case-template-hibernate-orm5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ test-case-template-hibernate-orm5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,11 +1,6 @@
 package org.hibernate.bugs;
 
-import java.util.Set;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
+import org.hibernate.Session;
 import org.hibernate.domain.HeadMaster;
 import org.hibernate.domain.Student;
 import org.hibernate.domain.Teacher;
@@ -13,83 +8,102 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.Set;
+
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
  */
 public class JPAUnitTestCase {
 
-	private EntityManagerFactory entityManagerFactory;
-	private EntityManager entityManager;
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
 
-	@Before
-	public void init() {
-		entityManagerFactory = Persistence.createEntityManagerFactory( "templatePU" );
-		entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-	}
+    @Before
+    public void init() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("templatePU");
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+    }
 
-	@After
-	public void destroy() {
-		entityManager.getTransaction().commit();
-		entityManager.close();
-		entityManagerFactory.close();
-	}
+    @After
+    public void destroy() {
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
 
-	// Entities are auto-discovered, so just add them anywhere on class-path
-	// Add your tests, using standard JUnit.
-	@Test
-	public void generateData() throws Exception {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		
-		Student stu = new Student();
-		stu.setName("RoseSL");
-		stu.setAge(41);
-		stu.setAddress("hahaKSL");
-		stu.setEmail("1234@145.com");
-		stu.setTelephone("4848449SD");
-		
-		Student stu2 = new Student();
-		stu2.setName("JackGG");
-		stu2.setAge(34);
-		stu2.setAddress("ojbk");
-		stu2.setEmail("kops@145.com");
-		stu2.setTelephone("9029102");
-		
-		Teacher tec = new Teacher();
-		tec.setName("JJKI");
-		tec.getStudents().add(stu);
-		tec.getStudents().add(stu2);
-		
-		HeadMaster hm = new HeadMaster();
-		hm.setName("X Professor");
-		hm.getStudents().add(stu);
-		hm.getStudents().add(stu2);
-		
-		stu.setTeacher(tec);
-		stu.setHeadMaster(hm);
-		stu2.setTeacher(tec);
-		stu2.setHeadMaster(hm);
-		
-		entityManager.persist(tec);
-		entityManager.persist(hm);
+    // Entities are auto-discovered, so just add them anywhere on class-path
+    // Add your tests, using standard JUnit.
+    @Test
+    public void generateData() throws Exception {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Student stu = new Student();
+        stu.setName("RoseSL");
+        stu.setAge(41);
+        stu.setAddress("hahaKSL");
+        stu.setEmail("1234@145.com");
+        stu.setTelephone("4848449SD");
+
+        Student stu2 = new Student();
+        stu2.setName("JackGG");
+        stu2.setAge(34);
+        stu2.setAddress("ojbk");
+        stu2.setEmail("kops@145.com");
+        stu2.setTelephone("9029102");
+
+        Teacher tec = new Teacher();
+        tec.setName("JJKI");
+        tec.getStudents().add(stu);
+        tec.getStudents().add(stu2);
+
+        HeadMaster hm = new HeadMaster();
+        hm.setName("X Professor");
+        hm.getStudents().add(stu);
+        hm.getStudents().add(stu2);
+
+        stu.setTeacher(tec);
+        stu.setHeadMaster(hm);
+        stu2.setTeacher(tec);
+        stu2.setHeadMaster(hm);
+
+        entityManager.persist(tec);
+        entityManager.persist(hm);
 //		
-		Student find = entityManager.find(Student.class, 1L);
-		System.out.println("db student : " + find);
-		
-		entityManager.getTransaction().commit();
-		entityManager.close();
-	}
-	
-//	@Test
-	public void deleteTest() {
-		Teacher teacher = entityManager.find(Teacher.class, 14);
-		Set<Student> tech_students = teacher.getStudents();
-		tech_students.stream().forEach(s -> entityManager.remove(s));
-		HeadMaster headMaster = entityManager.find(HeadMaster.class, 4);
-		headMaster.setName("Wolverine12");
-		
-		entityManager.persist(teacher);
-		entityManager.persist(headMaster);
-	}
+        Student find = entityManager.find(Student.class, 1L);
+        System.out.println("db student : " + find);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    //	@Test
+    public void deleteTest() {
+        Teacher teacher = entityManager.find(Teacher.class, 14);
+        Set<Student> tech_students = teacher.getStudents();
+        tech_students.stream().forEach(s -> entityManager.remove(s));
+        HeadMaster headMaster = entityManager.find(HeadMaster.class, 4);
+        headMaster.setName("Wolverine12");
+
+        entityManager.persist(teacher);
+        entityManager.persist(headMaster);
+    }
+
+    @Test
+    public void updateData() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        Session session = entityManager.unwrap(org.hibernate.Session.class);
+        Student student = session.get(Student.class, 1L);
+        student.setAddress("beijing");
+        System.out.println(student);
+        session.evict(student);
+        transaction.commit();
+    }
 }
--- test-case-template-hibernate-orm5/src/test/java/org/hibernate/domain/Student.java
+++ test-case-template-hibernate-orm5/src/test/java/org/hibernate/domain/Student.java
@@ -1,105 +1,105 @@
 package org.hibernate.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "student")
 public class Student {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Column(name = "name")
-	private String name;
-	@Column(name = "age")
-	private Integer age;
-	@Column(name = "address")
-	private String address;
-	@Column(name = "telephone")
-	private String telephone;
-	@Column(name = "email")
-	private String email;
-	@ManyToOne(targetEntity = Teacher.class)
-	@JoinColumn(name="teacher_id")
-	private Teacher teacher;
-	@ManyToOne(targetEntity = HeadMaster.class)
-	@JoinColumn(name="headmaster_id")
-	private HeadMaster headMaster;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private Integer age;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "telephone")
+    private String telephone;
+    @Column(name = "email")
+    private String email;
+    @ManyToOne(targetEntity = Teacher.class)
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+    @ManyToOne(targetEntity = HeadMaster.class)
+    @JoinColumn(name = "headmaster_id")
+    private HeadMaster headMaster;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Integer getAge() {
-		return age;
-	}
+    public Integer getAge() {
+        return age;
+    }
 
-	public void setAge(Integer age) {
-		this.age = age;
-	}
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public String getAddress() {
+        return address;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-	public String getTelephone() {
-		return telephone;
-	}
+    public String getTelephone() {
+        return telephone;
+    }
 
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public Teacher getTeacher() {
-		return teacher;
-	}
+    public Teacher getTeacher() {
+        return teacher;
+    }
 
-	public void setTeacher(Teacher teacher) {
-		this.teacher = teacher;
-	}
-	
-	public HeadMaster getHeadMaster() {
-		return headMaster;
-	}
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
 
-	public void setHeadMaster(HeadMaster headMaster) {
-		this.headMaster = headMaster;
-	}
+    public HeadMaster getHeadMaster() {
+        return headMaster;
+    }
 
-	@Override
-	public String toString() {
-		return "id:" + id + ", name:" + name + ",address:" + address
-				+ "telephone:" + telephone + ",email" + email;
-	}
+    public void setHeadMaster(HeadMaster headMaster) {
+        this.headMaster = headMaster;
+    }
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", email='" + email + '\'' +
+                ", teacher=" + teacher +
+                ", headMaster=" + headMaster +
+                '}';
+    }
 }
